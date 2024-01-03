e48ad774eaced249ae430d1a0474b4b70e52807e
frim-mnero <frimmnero@gmail.com>
2024-01-03 20:38:16 +0300

    starting

--- apps/mainApp.js
+++ apps/mainApp.js
@@ -1,7 +1,28 @@
+const btnMore = document.querySelectorAll(".questBtn");
+const questItem = document.querySelectorAll(".quest-item");
+const questItemDescr2 = document.querySelectorAll(".quest-item-descr2");
+
 const imgArrowBottom = document.querySelectorAll(".imgArrowBottom");
-const questBtn = document.querySelectorAll(".questBtn");
+const krestBtn = document.querySelectorAll(".krestBtn");
 
-questBtn.onclick = ()=>{
-    alert('clicl');
+let counter=1;
+
+for(let i = 0;i<=btnMore.length;i++){
+    btnMore[i].onclick=()=>{
+        counter+=1;
+        if(counter%2===0){
+            questItem[i].classList.add('quest-item-more');
+            questItemDescr2[i].classList.remove('hiddener');
+
+            imgArrowBottom[i].classList.add('hiddener');
+            krestBtn[i].classList.remove('hiddener');
+        }
+        else{
+            questItem[i].classList.remove('quest-item-more');
+            questItemDescr2[i].classList.add('hiddener');
+
+            imgArrowBottom[i].classList.remove('hiddener');
+            krestBtn[i].classList.add('hiddener');
+        }
+    }
 }
-
